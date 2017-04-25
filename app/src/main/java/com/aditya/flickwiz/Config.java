fb52c6e5af14fd7daa7a152f6a8157841b453190
Aditya Das <adityadas96@gmail.com>
2017-04-25 23:56:04 +0530

    improved UI

---
+++ app/src/main/java/com/aditya/flickwiz/Config.java
@@ -0,0 +1,18 @@
+package com.aditya.flickwiz;
+
+/**
+ * Created by Aditya on 25/04/17.
+ */
+
+
+/**
+ * Created by echessa on 1/13/17.
+ */
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyCn8I959tmToovGH8Y2l36qpMOixOC8lE8"	;
+
+}
